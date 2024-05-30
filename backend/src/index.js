8a5b12d461724a15d8df93245733910b0af2c552
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-30 10:42:47 +0530

    initialized the project

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const rootRouter = require("./router/index");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1", rootRouter);
+
+app.listen(4000,()=>{
+    console.log(`server started at port 4000`);
+})
